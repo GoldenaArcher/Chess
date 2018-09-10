5dd5631ca6849684b12d2eddd29f3182be23b202
GoldenArcher <luhan@rams.colostate.edu>
2018-09-10 10:36:34 -0600

    add comment for class Rook, and also add test class for Rook

--- src/Main/ChessPieces/Rook.java
+++ src/Main/ChessPieces/Rook.java
@@ -2,19 +2,38 @@ package Main.ChessPieces;
 
 import Main.ChessBoard;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * @author Lu Han
  */
-public class Rook extends  ChessPiece{
-    public Rook (ChessBoard board, Color color) {
+public class Rook extends ChessPiece {
+    public Rook(ChessBoard board, Color color) {
         super(board, color);
     }
 
+//    Given the incrementation, check all possible steps until meet the edge or another piece up or down
+    private ArrayList<String> upDown(int incrementation){
+        ArrayList <String> res = new ArrayList<>();
+        return res;
+    }
+
+    //    Given the incrementation, check all possible steps until meet the edge or another piece left or right
+    private ArrayList<String> leftRight(int incrementation){
+        ArrayList <String> res = new ArrayList<>();
+        return res;
+    }
+
+    /*
+    The Rook moves vertically or horizontally in a straight line. It can move forward or backward, left or right. Again,
+    like the King, the Rook can capture any enemy piece that is in its line of movement. In capturing, the Rook replaces
+    the enemy piece, which is removed from the board.
+     */
     @Override
     public ArrayList<String> legalMoves() {
-        return null;
+        ArrayList <String> res = new ArrayList<>();
+        return res;
     }
 
     @Override
---
+++ src/Test/ChessPieces/RookTest.java
@@ -0,0 +1,14 @@
+package Test.ChessPieces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static Main.ChessPieces.ChessPiece.Color.BLACK;
+import static Main.ChessPieces.ChessPiece.Color.WHITE;
+
+public class RookTest {
+
+    @Test
+    public void legalMoves() {
+    }
+}
